177807b0bdeddd7837425e7682075f5d3d17ff60
olivergoodman <ollygreatman@gmail.com>
2016-04-28 23:08:06 -0500

    first commit
    daniel/olly’s progress so far
    wrote classify in Node.py

---
+++ modules/node.py
@@ -0,0 +1,66 @@
+# DOCUMENTATION
+# =====================================
+# Class node attributes:
+# ----------------------------
+# children - a list of 2 nodes if numeric, and a dictionary (key=attribute value, value=node) if nominal.  
+#            For numeric, the 0 index holds examples < the splitting_value, the 
+#            index holds examples >= the splitting value
+#
+# label - is None if there is a decision attribute, and is the output label (0 or 1 for
+#	the homework data set) if there are no other attributes
+#       to split on or the data is homogenous
+#
+# decision_attribute - the index of the decision attribute being split on
+#
+# is_nominal - is the decision attribute nominal
+#
+# value - Ignore (not used, output class if any goes in label)
+#
+# splitting_value - if numeric, where to split
+#
+# name - name of the attribute being split on
+
+class Node:
+    def __init__(self):
+        # initialize all attributes
+        self.label = None
+        self.decision_attribute = None
+        self.is_nominal = None
+        self.value = None
+        self.splitting_value = None
+        self.children = {}
+        self.name = None
+
+    def classify(self, instance):
+        '''
+        given a single observation, will return the output of the tree
+        '''
+        curr = self
+        while curr.children:
+            if curr.is_nominal:
+                curr = curr.children[instance[curr.decision_attribute]]
+            else:
+                if instance[curr.decision_attribute] < curr.splitting_value:
+                    curr = curr.children[1]
+                else:
+                    curr = curr.children[2]
+        return curr.label
+
+    def print_tree(self, indent = 0):
+        '''
+        returns a string of the entire tree in human readable form
+        IMPLEMENTING THIS FUNCTION IS OPTIONAL
+        '''
+        # Your code here
+        pass
+        # use self.name to print
+
+
+    def print_dnf_tree(self):
+        '''
+        returns the disjunct normalized form of the tree.
+        '''
+        pass
+
+
+
---
+++ modules/pruning.py
@@ -0,0 +1,23 @@
+from node import Node
+from ID3 import *
+from operator import xor
+
+# Note, these functions are provided for your reference.  You will not be graded on their behavior,
+# so you can implement them as you choose or not implement them at all if you want to use a different
+# architecture for pruning.
+
+def reduced_error_pruning(root,training_set,validation_set):
+    '''
+    take the a node, training set, and validation set and returns the improved node.
+    You can implement this as you choose, but the goal is to remove some nodes such that doing so improves validation accuracy.
+    '''
+    # Your code here
+    pass
+# 
+
+def validation_accuracy(tree,validation_set):
+    '''
+    takes a tree and a validation set and returns the accuracy of the set on the given tree
+    '''
+    # Your code here
+    pass
